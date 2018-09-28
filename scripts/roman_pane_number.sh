05021faa12b439035c5c66a5e4acd14fd92c37a3
Nicholas Ireland <nick@nicholasireland.ca>
2018-09-27 21:18:08 -0400

    THIS SHOULD WORK

--- scripts/roman_pane_number.sh
+++ scripts/roman_pane_number.sh
@@ -4,8 +4,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 roman_pane_number() {
-   print_roman "#{pane_index}"
-   # print_roman $pane_index 
+   print_roman "$(tmux display-message -p | grep -E -o 'current pane [0-9]*' | grep -E -o '[0-9]+')"
 }
 
 roman_pane_number
