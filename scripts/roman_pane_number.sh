5bd6c53d5d89d3697f86ed64adcd4caa4f770720
Nicholas Ireland <nick@nicholasireland.ca>
2018-09-27 20:58:53 -0400

    see if this works

--- scripts/roman_pane_number.sh
+++ scripts/roman_pane_number.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 roman_pane_number() {
-   echo "#{pane_index}";
+   print_roman "#{pane_index}"
    # print_roman $pane_index 
 }
 
