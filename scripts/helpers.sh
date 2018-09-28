2ce70d122c17b9247fa8f6b4d08983df566983a5
Nicholas Ireland <nick@nicholasireland.ca>
2018-09-27 21:26:48 -0400

    test

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -32,16 +32,22 @@ command_exists() {
 }
 
 print_roman() {
-  local -a conv
-  local number=$1 div rom num out
-  conv=(I 1 IV 4 V 5 IX 9 X 10 XL 40 L 50 XC 90 C 100 CD 400 D 500 CM 900 M 1000)
-  for num rom in ${(Oa)conv}; do
-    (( div = number / num, number = number % num ))
-    while (( div-- > 0 )); do
-      out+=$rom
+    local values=( 1000 900 500 400 100 90 50 40 10 5 4 1 )
+    local roman=(
+        [1000]=M [900]=CM [500]=D [400]=CD 
+         [100]=C  [90]=XC  [50]=L  [40]=XL 
+          [10]=X   [9]=IX   [5]=V   [4]=IV   
+           [1]=I
+    )
+    local nvmber=""
+    local num=$1
+    for value in ${values[@]}; do
+        while (( num >= value )); do
+            nvmber+=${roman[value]}
+            ((num -= value))
+        done
     done
-  done
-  echo $out
+    echo $nvmber
 }
 
 round() {
--- scripts/roman_pane_number.sh
+++ scripts/roman_pane_number.sh
@@ -4,7 +4,8 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 roman_pane_number() {
-   print_roman "$(tmux display-message -p | grep -E -o 'current pane [0-9]*' | grep -E -o '[0-9]+')"
+    echo "test"
+   # print_roman "$(tmux display-message -p | grep -E -o 'current pane [0-9]*' | grep -E -o '[0-9]+')"
 }
 
 roman_pane_number
