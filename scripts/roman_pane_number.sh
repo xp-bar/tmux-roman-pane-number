b43a392171589805313f5ae8f19805095b184190
Nicholas Ireland <nick@nicholasireland.ca>
2018-09-27 20:51:10 -0400

    test

--- scripts/roman_pane_number.sh
+++ scripts/roman_pane_number.sh
@@ -4,7 +4,8 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 roman_pane_number() {
-   print_roman $pane_index 
+   echo "hi";
+   # print_roman $pane_index 
 }
 
 roman_pane_number
